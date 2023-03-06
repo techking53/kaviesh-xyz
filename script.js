2b1fa3c5d1b17b0a652e91726aa053c59f084f0a
techking53 <126987844+techking53@users.noreply.github.com>
2023-03-06 17:49:35 +0530

    typo

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ sliderNext.onclick=e=>{
 }
 
 javaBack.onclick=e=>{
-    javaphile.style.animation = "move-rightt 1s ease-in-out"
+    javaphile.style.animation = "move-right 1s ease-in-out"
     slider.style.animation = "come-from-left 1s ease-in-out"
     slider.classList.add("active")
     javaphile.style.transform = "translateX(100%)"
