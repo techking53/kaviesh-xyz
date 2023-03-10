81d792fca5ca60bb3d09911de3587427de392e75
techking53 <agarwaal@kaviesh.xyz>
2023-03-10 17:11:34 +0530

    cgp

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-document.querySelector("html").style.fontSize = window.innerWidth>645 ? window.innerWidth/153.6+"px" : window.innerWidth/92+"px"
+function rem() {
+    document.querySelector("html").style.fontSize = window.innerWidth>645 ? window.innerWidth/153.6+"px" : window.innerWidth/92+"px"
+}
+
+// Slider Effect
 
 const handleMouseMove =  e =>{
     document.querySelector("#left-side").style.width = (e.clientX / window.innerWidth * 100)+"%"
@@ -6,9 +10,79 @@ const handleMouseMove =  e =>{
 document.onmousemove=e=> handleMouseMove(e)
 document.ontouchmove=e=> handleMouseMove(e.touches[0])
 
+// Slider Effect
+
+
+// Stagger Effect
+
+const wrapper = document.getElementById("tiles");
+
+let columns = 0,
+rows = 0,
+toggled = false;
+
+const toggle = () => {
+    toggled = !toggled;
+    
+    document.querySelector(".tiles-c").classList.toggle("toggled");
+}
+const handleOnClick = index => {
+    toggle();
+    
+    anime({
+        targets: ".tile",
+        opacity: toggled ? 0 : 1,
+        delay: anime.stagger(50, {
+            grid: [columns, rows],
+            from: index
+        })
+    });
+}
+
+const createTile = index => {
+    const tile = document.createElement("div");
+    
+    tile.classList.add("tile");
+    
+    tile.style.opacity = toggled ? 0 : 1;
+    
+    tile.onclick = e => handleOnClick(index);
+    
+    return tile;
+}
+
+const createTiles = quantity => {
+    Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+    });
+}
+
+const createGrid = () => {
+    wrapper.innerHTML = "";
+    
+    const size = window.innerWidth > 800 ? 100 : 50;
+    
+    columns = Math.floor(window.innerWidth / size);
+    rows = Math.floor(window.innerHeight / size);
+    
+    wrapper.style.setProperty("--columns", columns);
+    wrapper.style.setProperty("--rows", rows);
+    
+    createTiles(columns * rows);
+}
+
+createGrid();
+window.onresize = () => {
+    createGrid()
+    rem()
+};
+
+// Stagger Effect
+
 const blanket=document.querySelector(".blanket")
 const slider=document.querySelector(".slider")
 const javaphile=document.querySelector(".javaphile")
+const cgp=document.querySelector(".cgp")
 
 function trans(pre,nex,an1,an2) {
     document.body.style.touchAction = "none"
@@ -33,24 +107,32 @@ function trans(pre,nex,an1,an2) {
         document.body.style.touchAction = "auto"
         document.querySelectorAll(".t-butt").forEach(e=>{e.classList.remove("disable")})
     },1100)
+    if (nex==javaphile) {
+        n=1
+        document.querySelectorAll(".j-iflex.me h3").forEach((e)=>{
+            e.classList.contains("anted") ? e.style.animation="none" : e.classList.add("anted")
+            edelay=(400+500*n)
+            e.style.animationDelay=edelay+"ms"
+            setTimeout((e) => {
+                e.style.transform="translateX(0)"
+            }, edelay+100,e);
+            n++
+        })
+        setTimeout(()=>{
+            blanket.classList.contains("animated") ? blanket.style.animation="none" : blanket.classList.add("animated")
+        },200)
+    }
 }
 
 slider.querySelector(".next").onclick=i=>{
     trans(slider,javaphile,"move-left","come-from-right")
-    n=1
-    document.querySelectorAll(".j-iflex.me h3").forEach((e)=>{
-        e.classList.contains("anted") ? e.style.animation="none" : e.classList.add("anted")
-        edelay=(400+500*n)
-        e.style.animationDelay=edelay+"ms"
-        setTimeout((e) => {
-            e.style.transform="translateX(0)"
-        }, edelay+100,e);
-        n++
-    })
-    setTimeout(()=>{
-        blanket.classList.contains("animated") ? blanket.style.animation="none" : blanket.classList.add("animated")
-    },200)
 }
 javaphile.querySelector(".back").onclick=i=>{
     trans(javaphile,slider,"move-right","come-from-left")
+}
+javaphile.querySelector(".next").onclick=i=>{
+    trans(javaphile,cgp,"move-up","come-from-down")
+}
+cgp.querySelector(".back").onclick=i=>{
+    trans(cgp,javaphile,"move-down","come-from-up")
 }
